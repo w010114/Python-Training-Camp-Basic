362bb6e0372aaa005c985e5c0a9dc793bf42dd05
w010114 <1804330964@qq.com>
2025-05-22 14:35:50 +0800

    完成math_module.py 练习

--- exercises/math_module.py
+++ exercises/math_module.py
@@ -7,6 +7,8 @@
 请补全下面的函数，使用math模块的sqrt函数计算平方根。
 """
 
+import math  # 导入 math 模块
+
 def calculate_square_root(number):
     """
     计算数字的平方根
@@ -18,4 +20,7 @@ def calculate_square_root(number):
     - 数字的平方根
     """
     # 请在下方编写代码
+    if number < 0:
+        return "错误：输入不能为负数"
+    return math.sqrt(number)
     pass 
