2614f6bd0f553ffd934819679d940d99e75bd105
w010114 <1804330964@qq.com>
2025-05-22 14:30:47 +0800

    完成 break_continue.py 练习

--- exercises/break_continue.py
+++ exercises/break_continue.py
@@ -18,4 +18,10 @@ def skip_multiples_of_three(n):
     - 从1到n中所有不是3的倍数的整数列表
     """
     # 请在下方编写代码
+    result = []
+    for i in range(1, n + 1):
+        if i % 3 == 0:
+            continue  # 跳过3的倍数
+        result.append(i)
+    return result
     pass 
